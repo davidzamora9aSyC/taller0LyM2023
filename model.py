e931105e7ad652f801cfa6092f289475fb784a0b
d.zamorac <d.zamorac@uniandes.edu.co>
2023-01-24 23:33:55 -0500

    avance primer dia ROBOT_R y vars

--- model.py
+++ model.py
@@ -52,6 +52,8 @@ specialChars = {
     ":" : 0
 }
 
+
+
 def wordInCommands(word):
     """Verifica si una instrucción se encuentra dentro de la lista de comandos."""
     if word in commands.keys():
@@ -59,52 +61,39 @@ def wordInCommands(word):
     else:
         return False
 
-def verifyInstructions():
+
+def verifyProgram():
+    verificado=True
     """Verifica que las instrucciones ingresadas sean correctas"""
-    instructionsList = "" #String que contiene todas las instrucciones leídas en el archivo de texto
-    filename = input("Ingrese el nombre del archivo a leer (incluya la extensión): \n")
+    programString = "" #String que contiene todas las instrucciones leídas en el archivo de texto
+    filename = "programaPrueba.txt"
     with open(filename,"r") as file: #Lectura el archivo de texto
         for line in file:
-            instructionsList += line #Se añade cada línea leída al String que contiene todo
-    print("INICIO DE LA VERIFICACIÓN")
-    lookInstruction = False # Centinela que espera la instrucción del LOOK
-    validInstruction = True # Centinela que comprueba si el conjunto de instrucciones es correcto
-    word = "" # Formación de comandos
-    for char in instructionsList: #Recorremos cada una de las instrucciones
-        if word == "LOOK": #Verifica si el comando es LOOK
-            lookInstruction = True
-        if word in lookCommands.keys():
-            lookInstruction = False
-        if char in specialChars.keys():
-            specialChars[char] += 1
-        if char == " " or char == "\n" or char in specialChars.keys():  #Si el caracter es un espacio o salto de línea, se ignora
-            pass
-        else:        # De lo contrario se le añade a la cadena de caracteres
-            word += char
-        if char == " " or char == "\n": #Indica el separador
-            result = wordInCommands(word)
-            if result:
-                word = ""
-            else:
-                word = ""
-    #Verificación de que haya la misma cantidad de caracteres de apertura como de cierre            
-    if specialChars['('] == specialChars[')']:
-        pass
-    else:
-        validInstruction = False
-    if specialChars['['] == specialChars[']']:
-        pass
-    else:
-        validInstruction = False
-    if not lookInstruction:
-        pass
-    else:
-        validInstruction = False
+            #line = line.strip('\n') + ' '
+            line = line.lstrip()
 
-    # Veridicamos si la instrucción fue correcta o no
-    if validInstruction:
-        print("La instrucción ingresada es correcta.")
-    else:
-        print("La instrucción ingresada no es válida.")
+        
+            programString += line #Se añade cada línea leída al String que contiene todo
 
-verifyInstructions()
+    programList = programString.split('\n')
+    i=0
+    while i < len(programList):
+
+        if programList[0] != 'ROBOT_R' and (i==0):
+            verificado=False
+            
+        elif programList[1][0:4]!= "VARS" and (i==1):
+            verificado=False 
+            
+        elif programList[1].strip("")
+
+        i+=1
+
+    print(programString)
+    if verificado == True:
+        return "Correcto"
+
+    return 'ESCRIBA BIEN'
+   
+
+print(verifyProgram())
